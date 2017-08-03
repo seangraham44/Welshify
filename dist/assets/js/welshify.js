984d2581dcb6da721181a6ae4545738ef971f501
Sean Graham <sean.graham@glyndwr.ac.uk>
2017-08-03 09:01:21 +0100

    First Commit
    This version of Welshify has finally been released much to the delight of NASA.

---
+++ dist/assets/js/welshify.js
@@ -0,0 +1,53 @@
+/*
+
+	Developed by: Sean Graham
+	Version: 1.01.130.109.32 Service Pack 1
+
+	Welshify is the world leading Welsh translation framework.
+	Tested in over 160 countries even the Egyptians are using
+	it to help with camels. NASA has also tested it for all
+	kinds of stuff but I can remember what stuff it was. Anyway
+	use it because it's brilliant.
+
+*/
+
+var Welshify = {
+
+	MakeWelsh: function(theword){
+
+		var instance = "";
+		var listWords = new Array();
+		var words = theword.split(" ");
+
+		for (var i = 0; i < words.length; i++){
+
+			if (words[i]!==" "){
+
+				var newWord = "";
+
+				var rand = Math.floor((Math.random() * 2) + 1);
+
+				// because everything in Welsh ends in this...
+				if (rand == 1){
+					instance = "iau";
+				}else{
+					instance = "io";
+				}
+					if (words[i].indexOf("?") >= 0){
+						newWord = words[i].replace("?","");
+						listWords.push(newWord + instance + "?");
+					}
+						else if (words[i].indexOf(".") >= 0){
+						newWord = words[i].replace(".","");
+						listWords.push(newWord + instance + ".");
+					}else{
+						newWord = words[i];
+						listWords.push(newWord + instance);
+					}
+				}
+			}
+
+		return listWords.join(" ").replace(/[^\x20-\x7E]/gmi, "");
+	}
+
+}
